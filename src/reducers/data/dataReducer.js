c3c828ffd0dec5fe460109f12190ddd88c9c3d42
Fredrik Fossum <fredrik.fossum@gmail.com>
2016-03-06 11:45:09 +0100

    Add data reducer

---
+++ src/reducers/data/dataReducer.js
@@ -0,0 +1,6 @@
+import {combineReducers} from 'redux';
+import users from './users/usersReducer';
+
+export default combineReducers({
+  users
+});
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import {combineReducers} from 'redux';
 import {routeReducer as routing} from 'react-router-redux';
 import forms from './forms';
 import loggedInUser from './loggedInUserReducer';
+import data from './data/dataReducer';
 
 export default combineReducers({
   routing,
+  data,
   forms,
   loggedInUser
 });
