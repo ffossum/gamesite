c4c072eebc9d19fa8e32ebe80306cda6362e60d4
Fredrik Fossum <fredrik.fossum@gmail.com>
2016-04-03 15:03:00 +0200

    Avoid duplicate user data rest calls

--- src/actions/userData.js
+++ src/actions/userData.js
@@ -3,6 +3,8 @@ import _ from 'lodash';
 
 export const GET_USER_DATA = 'userData/GET';
 
+const pendingFetches = {};
+
 export function getUserData(...userIds) {
   return (dispatch, getState) => {
     const stateUsers = getState().getIn(['data', 'users']);
@@ -12,19 +14,31 @@ export function getUserData(...userIds) {
       return;
     }
 
-    fetch(`/api/users?id=${missingUserIds.join(',')}`)
-      .then(res => {
-        res.json().then(json => {
-          if (res.ok) {
-            dispatch({
-              type: GET_USER_DATA,
-              payload: {
-                users: json.users,
-              },
+    missingUserIds.sort();
+    const fetchUrl = `/api/users?id=${missingUserIds.join(',')}`;
+
+    if (!pendingFetches[fetchUrl]) {
+      pendingFetches[fetchUrl] = true;
+
+      fetch(fetchUrl)
+        .then(res => {
+          res.json()
+            .then(json => {
+              if (res.ok) {
+                dispatch({
+                  type: GET_USER_DATA,
+                  payload: {
+                    users: json.users,
+                  },
+                });
+              }
+              delete pendingFetches[fetchUrl];
+            })
+            .catch(() => {
+              delete pendingFetches[fetchUrl];
             });
-          }
         });
-      });
+    }
   };
 }
 
