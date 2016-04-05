5c07ad45228ec343d73f073cc69ac59aec1e1496
Fredrik Fossum <fredrik.fossum@gmail.com>
2016-04-05 20:35:15 +0200

    Add istanbul code coverage

--- src/reducers/loggedInUserReducer.spec.js
+++ src/reducers/loggedInUserReducer.spec.js
@@ -1,7 +1,13 @@
 /* eslint-env mocha */
 
+import { expect } from 'chai';
+
+import loggedInUserReducer from './loggedInUserReducer';
+
+
 describe('loggedInUserReducer', () => {
-  it('works', () => {
-    // TODO
+  it('initializes to null', () => {
+    const initialState = loggedInUserReducer(undefined, { type: 'INIT' });
+    expect(initialState).to.be.null;
   });
 });
