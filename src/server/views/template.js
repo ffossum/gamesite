aab5e82761ecbaa22e465b197aa48851c32f0eed
Fredrik Fossum <fredrik.fossum@gmail.com>
2016-04-12 23:46:40 +0200

    Add html title

--- src/server/views/template.js
+++ src/server/views/template.js
@@ -22,6 +22,7 @@ export default ({
 <!doctype html>
 <html>
   <head>
+    <title>Game site</title>
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
     <link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,400italic,600,700' rel='stylesheet'>
