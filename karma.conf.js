b9802a0b37ad5b5db7c44f1474100c6864ba7beb
Fredrik Fossum <fredrik.fossum@gmail.com>
2016-04-12 19:23:50 +0200

    Fixed coverage

---
+++ karma.conf.js
@@ -0,0 +1,55 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = function (config) {
+  config.set({
+    browsers: ['PhantomJS'],
+    singleRun: true,
+    frameworks: ['mocha'],
+    files: [
+      'src/**/*.spec.js',
+    ],
+    preprocessors: {
+      'src/**/*.js': ['webpack'],
+    },
+    reporters: ['mocha', 'coverage'],
+    webpack: {
+      resolve: {
+        modulesDirectories: [
+          'src',
+          'node_modules',
+        ],
+        extensions: ['', '.json', '.js'],
+      },
+      module: {
+        preLoaders: [
+          {
+            test: /\.spec\.js$/,
+            include: path.join(__dirname, 'src'),
+            exclude: /node_modules/,
+            loader: 'babel',
+          },
+          {
+            test: /\.js?$/,
+            include: path.join(__dirname, 'src'),
+            exclude: /(node_modules|\.spec\.js$)/,
+            loader: 'babel-istanbul',
+          },
+        ],
+        loaders: [
+          { test: /\.json$/, loaders: ['json'] },
+          { test: /\.js$/, loader: 'babel', exclude: /node_modules/ },
+          { test: /\.css$/, loader: 'css-loader/locals!postcss-loader' },
+        ],
+      },
+    },
+    webpackServer: {
+      noInfo: true,
+    },
+    coverageReporter: {
+      type: 'lcov',
+      dir: 'test/coverage/',
+      subdir: '.',
+    },
+  });
+};
--- src/containers/Nav.spec.js
+++ src/containers/Nav.spec.js
@@ -1,8 +1,12 @@
 /* eslint-env mocha */
 /* eslint no-unused-expressions: 0 */
 
+import Nav from './Nav';
+import { expect } from 'chai';
+
 describe('Nav', () => {
   it('works', () => {
+    expect(Nav).not.to.be.null;
     // TODO
   });
 });
