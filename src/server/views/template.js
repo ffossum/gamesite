522932c2c48cde9c7056ac28c59754b0becc689b
Fredrik Fossum <fredrik.fossum@gmail.com>
2016-04-20 23:49:01 +0200

    Add Lato font for headings

--- src/server/views/template.js
+++ src/server/views/template.js
@@ -26,6 +26,7 @@ export default ({
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
     <link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,400italic,600,700' rel='stylesheet'>
+    <link href='https://fonts.googleapis.com/css?family=Lato:900' rel='stylesheet' type='text/css'>
     <script>window.__INITIAL_STATE__ = ${initialState};</script>
 
     ${__DEVELOPMENT__ ? devScripts : prodScripts}
