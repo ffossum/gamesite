2213710607a3780edf5757177dffb062d24039c7
Fredrik Fossum <fredrik.fossum@gmail.com>
2016-05-13 20:49:22 +0200

    Keep hand history scrolled to bottom

--- src/games/rps/components/HandHistory.js
+++ src/games/rps/components/HandHistory.js
@@ -5,6 +5,7 @@ import {
 } from 'lodash';
 import Hand from './hands/Hand';
 import Gravatar from 'components/common/Gravatar';
+import KeepBottomScroll from 'components/common/KeepBottomScroll';
 
 import styles from './handHistory.css';
 
@@ -24,25 +25,27 @@ export default class HandHistory extends React.Component {
             ))
           }
         </div>
-        <div className={styles.body}>
-            {
-              map(history, (hands, rowIndex) => (
-                <div key={rowIndex} className={styles.row}>
-                  {
-                    map(hands, (hand, cellIndex) => (
-                      <span key={cellIndex} className={styles.cell}>
-                        <Hand
-                          small
-                          type={hand}
-                          direction={cellIndex === 0 ? 'right' : 'left'}
-                        />
-                      </span>
-                    ))
-                  }
-                </div>
-              ))
-            }
-        </div>
+        <KeepBottomScroll>
+          <div className={styles.body}>
+              {
+                map(history, (hands, rowIndex) => (
+                  <div key={rowIndex} className={styles.row}>
+                    {
+                      map(hands, (hand, cellIndex) => (
+                        <span key={cellIndex} className={styles.cell}>
+                          <Hand
+                            small
+                            type={hand}
+                            direction={cellIndex === 0 ? 'right' : 'left'}
+                          />
+                        </span>
+                      ))
+                    }
+                  </div>
+                ))
+              }
+          </div>
+        </KeepBottomScroll>
         <div className={`${styles.row} ${styles.footer}`}>
           {
             map(players, player => (
