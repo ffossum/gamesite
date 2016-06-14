0f3c6a29e3a1e61a2d528bd1774ffaa031481ef5
Fredrik Fossum <fredrik.fossum@gmail.com>
2016-06-14 20:45:26 +0200

    Updated about page text

--- src/components/About.js
+++ src/components/About.js
@@ -9,6 +9,16 @@ export default class About extends React.Component {
         <header>
           <h1>About</h1>
         </header>
+        <p>
+          This site is a work in progress
+          and rock-paper-scissors is meant to be replaced by something more interesting.
+        </p>
+        <p>
+          Created by Fredrik Fossum.
+        </p>
+        <p>
+          Code is open source and available at <a href="https://github.com/ffossum/gamesite">GitHub</a>.
+        </p>
       </section>
     );
   }
