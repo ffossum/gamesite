a204bc0c8c6ed842b69a01c12bd815af995dcaf8
Fredrik Fossum <fredrik.fossum@gmail.com>
2016-06-18 18:55:41 +0200

    Simplify CreateGameContainer

--- src/containers/CreateGameContainer.js
+++ src/containers/CreateGameContainer.js
@@ -1,28 +1,11 @@
-import React, { PropTypes } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import CreateGame from 'components/CreateGame';
 import actions from 'actions/gamesList';
 
-class Wrapper extends React.Component {
-  render() {
-    return (
-      <CreateGame
-        createGame={this.props.createGame}
-        disabled={!this.props.userId}
-      />
-    );
-  }
-}
-
-Wrapper.propTypes = {
-  createGame: PropTypes.func.isRequired,
-  userId: PropTypes.string,
-};
-
 export default connect(
   state => ({
-    userId: state.getIn(['session', 'userId']),
+    disabled: !state.getIn(['session', 'userId']),
   }),
   dispatch => bindActionCreators(actions, dispatch)
-)(Wrapper);
+)(CreateGame);
