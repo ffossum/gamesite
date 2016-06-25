71d4d79f2820de41acd4f1105a78669f5dba0042
Fredrik Fossum <fredrik.fossum@gmail.com>
2016-06-26 01:22:32 +0200

    Remove init db console spam

--- src/server/db/init.js
+++ src/server/db/init.js
@@ -1,4 +1,4 @@
-import _ from 'lodash';
+import _, { noop } from 'lodash';
 import r from 'rethinkdb';
 import connect from './connect';
 
@@ -7,20 +7,18 @@ async function initRdb() {
   try {
     conn = await connect();
 
-    await r.dbCreate('test').run(conn).error(console.log);
+    await r.dbCreate('test').run(conn).error(noop);
 
     await Promise.all([
-      await r.tableCreate('users').run(conn).error(console.log),
-      await r.tableCreate('games').run(conn).error(console.log),
-      await r.tableCreate('users_games').run(conn).error(console.log),
+      await r.tableCreate('users').run(conn).error(noop),
+      await r.tableCreate('games').run(conn).error(noop),
+      await r.tableCreate('users_games').run(conn).error(noop),
     ]);
 
-    await r.table('users').indexCreate('username').run(conn).error(console.log);
-    await r.table('users_games').indexCreate('userId').run(conn).error(console.log);
-    await r.table('users_games').indexCreate('gameId').run(conn).error(console.log);
-    await r.table('users').indexWait().run(conn).error(console.log);
-
-    console.log('connected');
+    await r.table('users').indexCreate('username').run(conn).error(noop);
+    await r.table('users_games').indexCreate('userId').run(conn).error(noop);
+    await r.table('users_games').indexCreate('gameId').run(conn).error(noop);
+    await r.table('users').indexWait().run(conn).error(noop);
   } catch (err) {
     console.log('error', err);
   }
