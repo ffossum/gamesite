9cb5a5cd930dba62cd89a09135db91a66d07b236
Fredrik Fossum <fredrik.fossum@gmail.com>
2016-07-26 15:12:32 +0200

    Add canceled game status text

--- src/constants/gameStatusText.js
+++ src/constants/gameStatusText.js
@@ -2,6 +2,7 @@ import {
     NOT_STARTED,
     IN_PROGRESS,
     ENDED,
+    CANCELED,
 } from './gameStatus';
 
 export default function getText(status) {
@@ -9,6 +10,7 @@ export default function getText(status) {
     case NOT_STARTED: return 'Not started';
     case IN_PROGRESS: return 'In progress';
     case ENDED: return 'Ended';
+    case CANCELED: return 'Canceled';
     default: return '';
   }
 }
