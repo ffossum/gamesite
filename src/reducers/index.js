df0d35edffd878d0d1035dae06542e8f80b788de
Fredrik Fossum <fredrik.fossum@gmail.com>
2016-07-27 13:33:29 +0200

    Add lobbyReducer to track last updated

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,11 +5,13 @@ import session from './session/sessionReducer';
 import data from './data/dataReducer';
 import mainChat from './mainChat';
 import modal from './modal';
+import lobby from './lobby/lobbyReducer';
 
 export default combineReducers({
   routing,
   data,
   mainChat,
+  lobby,
   forms,
   session,
   modal,
