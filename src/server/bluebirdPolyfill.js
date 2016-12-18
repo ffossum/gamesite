098e152a52e53ee59eb9d9692b14f83a7f88e273
Fredrik Fossum <fredrik.fossum@gmail.com>
2016-12-18 02:11:57 +0100

    Update bcrypt to v1

--- src/server/bluebirdPolyfill.js
+++ src/server/bluebirdPolyfill.js
@@ -1,8 +1,6 @@
 import bluebird from 'bluebird';
-import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 
 global.Promise = bluebird;
 
-Promise.promisifyAll(bcrypt);
 Promise.promisifyAll(jwt);
--- src/server/crypto.js
+++ src/server/crypto.js
@@ -2,10 +2,10 @@ import bcrypt from 'bcrypt';
 
 const BCRYPT_SALT_ROUNDS = 10;
 
-export async function hashPassword(password) {
-  return await bcrypt.hashAsync(password, BCRYPT_SALT_ROUNDS);
+export function hashPassword(password) {
+  return bcrypt.hash(password, BCRYPT_SALT_ROUNDS);
 }
 
-export async function comparePassword(password, hash) {
-  return await bcrypt.compareAsync(password, hash);
+export function comparePassword(password, hash) {
+  return bcrypt.compare(password, hash);
 }
