4ea0f092f5335a4244a7e8cbca242b2b1573cd48
Fredrik Fossum <fredrik.fossum@gmail.com>
2016-12-23 20:02:42 +0100

    Disable async loading of game component for now, to avoid client/server rendered react mismatch

--- src/components/gameRoom/GameInProgress.js
+++ src/components/gameRoom/GameInProgress.js
@@ -1,35 +1,16 @@
 import React, { PropTypes } from 'react';
 
 import styles from './gameRoom.css';
-
-let GameComponent;
-function getGameComponentAsync() {
-  return new Promise((resolve) => {
-    require.ensure([], require => {
-      GameComponent = require('games/rps').Game;
-      resolve(GameComponent);
-    });
-  });
-}
+import { Game as RockPaperScissors } from 'games/rps';
 
 export default class GameInProgress extends React.Component {
-  componentWillMount() {
-    if (!GameComponent) {
-      getGameComponentAsync().then(() => {
-        this.forceUpdate();
-      });
-    }
-  }
   render() {
     const { game, user, sendGameMessage } = this.props;
 
-    if (!GameComponent) {
-      return <div>Loading game...</div>;
-    }
     return (
       <div className={styles.inProgress}>
         <div className={styles.gameWrapper}>
-          <GameComponent user={user} game={game} sendMessage={sendGameMessage} />
+          <RockPaperScissors user={user} game={game} sendMessage={sendGameMessage} />
         </div>
       </div>
     );
