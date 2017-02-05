fde48e1e09502d6b4e1ea3c2acc2ba7be3b88e6e
Fredrik Fossum <fredrik.fossum@gmail.com>
2017-02-05 01:05:55 +0100

    Update react

--- src/server/views/template.js
+++ src/server/views/template.js
@@ -7,8 +7,8 @@ function getHeadContent() {
   const assets = JSON.parse(fs.readFileSync('dist/webpack-assets.json', 'utf8'));
   return `
     <link rel="stylesheet" href="${assets.main.css}"></link>
-    <script src="https://cdn.jsdelivr.net/react/15.4.1/react-with-addons.min.js" defer></script>
-    <script src="https://cdn.jsdelivr.net/react/15.4.1/react-dom.min.js" defer></script>
+    <script src="https://cdn.jsdelivr.net/react/15.4.2/react-with-addons.min.js" defer></script>
+    <script src="https://cdn.jsdelivr.net/react/15.4.2/react-dom.min.js" defer></script>
     <script src="https://cdn.jsdelivr.net/g/lodash@4.17.3(lodash.min.js+lodash.fp.min.js)" defer></script>
     <script src="https://cdn.jsdelivr.net/momentjs/2.14.1/moment.min.js" defer></script>
     <script src="${assets.main.js}" defer></script>
